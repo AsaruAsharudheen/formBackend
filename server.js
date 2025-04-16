aa0a338756343e3a559684e1e0486930d82b1edf
AsaruAsharudheen <asaru313313@gmail.com>
2025-04-16 22:53:47 +0530

    adding server json

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ require('dotenv').config('./.env');
 const Details = require('./db/details-schema');
 
 const App = express();
-
-
+const port = process.env.PORT || 4009;
 
 App.use(express.json());
 App.use(cors());
@@ -25,6 +24,6 @@ App.post('/Details', async (req, res) => {
   res.status(200).json(Detail);
 });
 
-App.listen(4009, () => {
+App.listen(port, () => {
   console.log(`App is running`);
 });
