7672847df64668528b0f649332e0d5557eef95a7
AsaruAsharudheen <asaru313313@gmail.com>
2025-04-16 23:09:47 +0530

    adding index json

--- db/index.js
+++ db/index.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 mongoose
-  .connect('mongodb://localhost:27017/MSFDB')
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('DB CONNECTED');
   })
   .catch(e => {
-    console.log(e);
+    console.error('DB CONNECTION ERROR:', e);
   });
+
 module.exports = mongoose;
